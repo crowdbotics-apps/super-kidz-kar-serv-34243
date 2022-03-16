2ee8974a1a1fd049928eb644d648ebacaefecdb3
Crowdbotics <team@crowdbotics.com>
2022-03-16 22:47:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "super_kidz_kar_serv_34243",
+  url: "https://super_kidz_kar_serv_34243.botics.co",
+  api: "https://super_kidz_kar_serv_34243.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
